af50ae4050be138fb0cc5436efd6b02df4fad818
Ahmad Qureshi <aqureshi536@gmail.com>
2016-10-23 22:28:42 +0530

    learnt to create class and its object

--- .worksheet/src/FirstCode.scala
+++ .worksheet/src/FirstCode.scala
@@ -1,11 +1,14 @@
 object FirstCode {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(62); 
-  println("Welcome to the Scala worksheet");$skip(115); 
+  println("Welcome to the Scala worksheet");$skip(133); 
 
   // Creating a variable but every thing is object even the + is a function as scala is functional
-  var num = 3;System.out.println("""num  : Int = """ + $show(num ));$skip(15); 
-  var num1 = 5;System.out.println("""num1  : Int = """ + $show(num1 ));$skip(22); 
-  println(num + num1)
+  var num = 3;System.out.println("""num  : Int = """ + $show(num ));$skip(157);  // assign a value
+  						//automatically it will take the type else
+  var num1 :Int = 5;System.out.println("""num1  : Int = """ + $show(num1 ));$skip(46); ; // Specifying this defines the datatype but there are no primitive datatype in scala
+  println(num + num1);// to print a statemant
 
+// like java "final" keyword ,"val" keyword in scala should be used to make it constant
+// "var" in scala is something that can be changed
 
 
 /* Creating a class with contructor
@@ -18,11 +21,15 @@ object FirstCode {;import org.scalaide.worksheet.runtime.library.WorksheetSuppor
 	 2 - var eobj = Employee(102); // wiht only one value if its the first parameter else
 	 3 - var eobj = Employee(eName = "Aamir"); // Incase you are assigning the vakue to a diffrent parameter
 	 																							which is not first
+	 4 - var eobj = Employee(104,"Aamir");  // Incase you want to change the default values
 */
+
   case class Employee(var eId: Int = 101, var eName: String = "Ahmad") {
+//Class defination
+  };$skip(888); 
 
-  };$skip(606); 
 
+// Object Creation wiht name "eObj"
   var eObj = Employee();;System.out.println("""eObj  : FirstCode.Employee = """ + $show(eObj ))}
 
 }
--- bin/FirstCode.sc
+++ bin/FirstCode.sc
@@ -1,11 +1,15 @@
 object FirstCode {
-  println("Welcome to the Scala worksheet")
+  println("Welcome to the Scala worksheet")       //> Welcome to the Scala worksheet
 
   // Creating a variable but every thing is object even the + is a function as scala is functional
-  var num = 3
-  var num1 = 5
-  println(num + num1)
+  var num = 3 // assign a value                   //> num  : Int = 3
+  						//automatically it will take the type else
+  var num1 :Int = 5; // Specifying this defines the datatype but there are no primitive datatype in scala
+                                                  //> num1  : Int = 5
+  println(num + num1);// to print a statemant     //> 8
 
+// like java "final" keyword ,"val" keyword in scala should be used to make it constant
+// "var" in scala is something that can be changed
 
 
 /* Creating a class with contructor
@@ -18,11 +22,15 @@ object FirstCode {
 	 2 - var eobj = Employee(102); // wiht only one value if its the first parameter else
 	 3 - var eobj = Employee(eName = "Aamir"); // Incase you are assigning the vakue to a diffrent parameter
 	 																							which is not first
+	 4 - var eobj = Employee(104,"Aamir");  // Incase you want to change the default values
 */
-  case class Employee(var eId: Int = 101, var eName: String = "Ahmad") {
 
+  case class Employee(var eId: Int = 101, var eName: String = "Ahmad") {
+//Class defination
   }
 
-  var eObj = Employee();
+
+// Object Creation wiht name "eObj"
+  var eObj = Employee();                          //> eObj  : FirstCode.Employee = Employee(101,Ahmad)
 
 }
